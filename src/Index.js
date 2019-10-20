9761f344ca663429b80765da42ca66d35809a11d
Aronja <anovicky@thoughtworks.com>
2019-10-20 21:48:13 +0100

    Add style-loader

--- src/Index.js
+++ src/Index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import './css/header.css'
 import App from "./App";
-
+import "./css/header.css"
 
 render(
   <App />, document.getElementById('app')
